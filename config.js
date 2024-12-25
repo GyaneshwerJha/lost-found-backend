41b2dcdfe77b09a4f2134e63542e82271b27fb37
GyaneshwerJha <keshavk72411@gmail.com>
2024-12-25 17:45:47 +0530

    backend-completed

---
+++ config.js
@@ -0,0 +1,2 @@
+export const PORT = process.env.PORT || 8000;
+export const mongoURL ="mongodb+srv://gyaneshwer0001:gyaneshwer0001@cluster0.zpbmyjw.mongodb.net/Lost&Found?retryWrites=true&w=majority&appName=Cluster0"; 
